d164a63b1ca50524d09f853f17f0734ec73d8c2c
dahn0209 <dahn0209@gmail.com>
2023-12-26 03:49:45 -0500

    added chats route

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express=require("express");
+const {chats}=require("./data/data")
 
 const app=express();
 
@@ -7,5 +8,8 @@ app.get("/",(req,res)=> {        ////2nd paramenter is callback with requestn an
     res.send("API is running");
 });
 
+app.get("/api/chats",(req,res)=>{
+    res.send(chats)    
+})
 
 app.listen(5001,console.log("Server is on PORT 5001"))   ////running npm start will run this
