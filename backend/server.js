5ef8b050ae393ee25e38429860c843492c4d7bb6
dahn0209 <dahn0209@gmail.com>
2023-12-27 02:47:24 -0500

    single chats api

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 const express=require("express");
+const dotenv=require("dotenv")
 const {chats}=require("./data/data")
 
 const app=express();
+dotenv.config()
 
 ////creating API 
 app.get("/",(req,res)=> {        ////2nd paramenter is callback with requestn and respond in the parament of callback functoin
-    res.send("API is running");
+    res.send("API is running great");
 });
 
-app.get("/api/chats",(req,res)=>{
+app.get("/api/chats",(req,res)=>{      ///this send  out chat data that's imported in///
     res.send(chats)    
 })
 
-app.listen(5001,console.log("Server is on PORT 5001"))   ////running npm start will run this
+app.get("/api/chats/:id",(req,res)=>{      ///this is for single chat
+    const singleChat=chats.find(c=>c._id=== req.params.id);
+    res.send(singleChat)
+})
+
+const PORT=process.env.PORT|| 5000;
+
+app.listen(5001,console.log(`Server starts at ${PORT}`))  ////running npm start will run this//
