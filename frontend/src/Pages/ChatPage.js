efd4bd366922bedf9466825feeafacfa1de7e137
dahn0209 <dahn0209@gmail.com>
2024-02-14 04:09:32 -0500

    axios

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 const ChatPage = () => {
 
-  const fetchChats=()=>{
-    
+  const fetchChats=async ()=>{
+    const {data}= await axios.get('/api/chat')
+    console.log('data=>',data)
   }
 
+  ////useEffect -->hook that runs when component is rendered for first time
+
+  useEffect(()=>{
+    fetchChats()
+  },[])
 
   return (
     <div>
